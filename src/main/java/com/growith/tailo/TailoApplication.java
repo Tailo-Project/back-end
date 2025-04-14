b93dadca04fe7185569632ec20a2b00fa1f938be
MinJu Kim <rorrxr@naver.com>
2025-04-14 21:16:33 +0900

    chore : 프로젝트 초기 설정

---
+++ src/main/java/com/growith/tailo/TailoApplication.java
@@ -0,0 +1,13 @@
+package com.growith.tailo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TailoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TailoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/growith/tailo/TailoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.growith.tailo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TailoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
