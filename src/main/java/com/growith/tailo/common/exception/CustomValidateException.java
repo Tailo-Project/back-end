19500a86bb7b69a3fa92dbf7a344cef97b9b5720
MinJu Kim <rorrxr@naver.com>
2025-04-15 15:50:58 +0900

    feat : common Handler, Exception, Response 생성

---
+++ src/main/java/com/growith/tailo/common/exception/CustomValidateException.java
@@ -0,0 +1,16 @@
+package com.growith.tailo.common.exception;
+
+public class CustomValidateException extends RuntimeException {
+
+    public CustomValidateException() {
+        super("The request is invalid, validation failed.");
+    }
+
+    public CustomValidateException(String message) {
+        super(message);
+    }
+
+    public CustomValidateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
