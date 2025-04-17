74c20bc213720eb10c6120bd2a6a560d57d9528e
OYEs <160278000+yjuni0@users.noreply.github.com>
2025-04-17 19:18:36 +0900

    rename: member file 부분 삭제

--- src/main/java/com/growith/tailo/member/dto/request/SignUpRequest.java
+++ src/main/java/com/growith/tailo/member/dto/request/SignUpRequest.java
@@ -4,5 +4,5 @@ import com.growith.tailo.member.enums.GenderType;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
 
-public record SignUpRequest(String email, String accountId, String nickname, String type, String breed, Integer age, GenderType gender, String address, MultipartFile file) {
+public record SignUpRequest(String email, String accountId, String nickname, String type, String breed, Integer age, GenderType gender, String address) {
 }
