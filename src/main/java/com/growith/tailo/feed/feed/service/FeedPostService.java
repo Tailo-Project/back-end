8467109e9b4262fd57cd9d4c1bca03617918da66
yeon-so <115128881+yeon-so@users.noreply.github.com>
2025-04-19 09:45:56 +0900

    feat: 나와 나의 팔로우 피드 목록 조회 구현 (#64)
    * fix: 유효성 검사 미적용 이슈 해결
    
    * feet: 권한 없음 예외 공통화 처리
    
    * feat: 피드 수정 api 추가
    
    feat: 피드 본문 수정
    
    * feat: 피드 수정 중 이미지 처리 로직 구현
    
    * feat: 이미지 삭제 구현
    
    * feat: 해시태그 수정 구현
    
    * fix: 일부 로직 수정
    
    * refactor: 로그 수정
    
    * feat: 나와 나의 팔로우 피드 목록 조회
    
    * Remove generated files from Git and update .gitignore

--- src/main/java/com/growith/tailo/feed/feed/service/FeedPostService.java
+++ src/main/java/com/growith/tailo/feed/feed/service/FeedPostService.java
@@ -2,7 +2,11 @@ package com.growith.tailo.feed.feed.service;
 
 import com.growith.tailo.feed.feed.dto.request.FeedPostRequest;
 import com.growith.tailo.feed.feed.dto.request.FeedUpdateRequest;
+import com.growith.tailo.feed.feed.dto.response.FeedPostResponse;
+
 import com.growith.tailo.member.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -12,5 +16,7 @@ public interface FeedPostService {
     String registerFeedPost(FeedPostRequest feedRegisterRequest, Member member, List<MultipartFile> images);
 
     String updateFeedPost(Long feedNumber, FeedUpdateRequest feedUpdateRequest, Member member, List<MultipartFile> images);
+
+    Page<FeedPostResponse> getFeedPostList(Member member, Pageable pageable);
 
 }
