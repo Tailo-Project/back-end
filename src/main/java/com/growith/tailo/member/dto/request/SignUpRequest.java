fae4c1af5615c30e6d32839670d0d8803118d78a
OYEs <160278000+yjuni0@users.noreply.github.com>
2025-04-19 15:35:48 +0900

    rename:띄어쓰기 추가

--- src/main/java/com/growith/tailo/member/dto/request/SignUpRequest.java
+++ src/main/java/com/growith/tailo/member/dto/request/SignUpRequest.java
@@ -4,5 +4,12 @@ import com.growith.tailo.member.enums.GenderType;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
 
-public record SignUpRequest(String email, String accountId, String nickname, String type, String breed, Integer age, GenderType gender, String address) {
+public record SignUpRequest(String email,
+                            String accountId,
+                            String nickname,
+                            String type,
+                            String breed,
+                            Integer age,
+                            GenderType gender,
+                            String address) {
 }
