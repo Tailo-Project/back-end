b74e4486dade677a97bb4eaa16e7b0a51c80b28b
OYEs <160278000+yjuni0@users.noreply.github.com>
2025-04-21 21:51:06 +0900

    docs: 파일 보류

---
+++ src/main/java/com/growith/tailo/security/jwt/controller/NewAccessTokenController.java
@@ -0,0 +1,21 @@
+//package com.growith.tailo.security.jwt.controller;
+//
+//import com.growith.tailo.member.dto.response.LoginResponse;
+//import com.growith.tailo.security.jwt.repository.RefreshTokenRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.*;
+
+////프론트에서 요청할 수 있게 한다면 작성
+
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/api/token")
+//public class NewAccessTokenController {
+//    private final RefreshTokenRepository refreshTokenRepository;
+//
+//    @GetMapping("/{accessToken}")
+//    public LoginResponse newAccessToken(@PathVariable String accessToken ){
+//
+//    }
+//
+//}
