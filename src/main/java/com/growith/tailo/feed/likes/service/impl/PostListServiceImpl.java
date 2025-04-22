53d36840b6c6fa3e8569568948584f884aea09b7
yeon-so <nordstromm03@gmail.com>
2025-04-22 23:22:05 +0900

    fix: 분산락 적용

--- src/main/java/com/growith/tailo/feed/likes/service/impl/PostListServiceImpl.java
+++ src/main/java/com/growith/tailo/feed/likes/service/impl/PostListServiceImpl.java
@@ -9,14 +9,19 @@ import com.growith.tailo.feed.likes.service.PostListService;
 import com.growith.tailo.member.entity.Member;
 import com.growith.tailo.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class PostListServiceImpl implements PostListService {
 
+    private final RedissonClient redissonClient;
     private final MemberRepository memberRepository;
     private final FeedPostRepository feedPostRepository;
     private final PostLikeRepository postLikeRepository;
@@ -25,29 +30,46 @@ public class PostListServiceImpl implements PostListService {
     @Transactional
     public String likeFeedPost(Long feedId, Member member) {
 
-        if (member == null || !memberRepository.existsByAccountId(member.getAccountId())) {
-            throw new ResourceNotFoundException("해당 회원이 존재하지 않습니다.");
-        }
+        String lockKey = "LOCK:feed:" + feedId;
+        RLock lock = redissonClient.getLock(lockKey);
 
-        FeedPost feedPost = feedPostRepository.findById(feedId)
-                .orElseThrow(() -> new ResourceNotFoundException("존재하지 않는 피드입니다."));
+        try {
+            boolean available = lock.tryLock(5, 3, TimeUnit.SECONDS);
+            if (!available) {
+                throw new RuntimeException("좋아요 락 획득 실패: 다른 사용자가 처리 중입니다.");
+            }
 
-        boolean isAlreadyLiked = postLikeRepository.existsByMemberAndFeedPost(member, feedPost);
+            if (member == null || !memberRepository.existsByAccountId(member.getAccountId())) {
+                throw new ResourceNotFoundException("해당 회원이 존재하지 않습니다.");
+            }
 
-        if (isAlreadyLiked) {
-            postLikeRepository.deleteByMemberAndFeedPost(member, feedPost);
-            return "좋아요 취소";
+            FeedPost feedPost = feedPostRepository.findById(feedId)
+                    .orElseThrow(() -> new ResourceNotFoundException("존재하지 않는 피드입니다."));
 
-        } else {
+            boolean isAlreadyLiked = postLikeRepository.existsByMemberAndFeedPost(member, feedPost);
 
-            PostLike postLike = PostLike.builder()
-                    .member(member)
-                    .feedPost(feedPost)
-                    .build();
+            if (isAlreadyLiked) {
+                postLikeRepository.deleteByMemberAndFeedPost(member, feedPost);
+                return "좋아요 취소";
 
-            postLikeRepository.save(postLike);
+            } else {
 
-            return "좋아요 성공";
+                PostLike postLike = PostLike.builder()
+                        .member(member)
+                        .feedPost(feedPost)
+                        .build();
+
+                postLikeRepository.save(postLike);
+
+                return "좋아요 성공";
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("락 대기 중 인터럽트 발생", e);
+        } finally {
+            if (lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
         }
 
     }
