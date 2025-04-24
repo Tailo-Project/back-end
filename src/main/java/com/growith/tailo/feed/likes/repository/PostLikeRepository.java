5acc8164d18cf8906c02021777a3f91d11030b71
yeon-so <nordstromm03@gmail.com>
2025-04-24 16:18:02 +0900

    chore: @Repository 명시

--- src/main/java/com/growith/tailo/feed/likes/repository/PostLikeRepository.java
+++ src/main/java/com/growith/tailo/feed/likes/repository/PostLikeRepository.java
@@ -6,9 +6,11 @@ import com.growith.tailo.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
     void deleteByFeedPost(FeedPost feedPost);
 
